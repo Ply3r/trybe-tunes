606b28325b8f91150476fa99c3c46dcfc49aa463
Leandro <play3rtox1c@gmail.com>
2021-10-21 09:21:58 -0300

    music api

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -1,5 +1,6 @@
 const getMusics = async (id) => {
-  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
+  const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`,
+  { mode: 'cors', method: 'post', headers: { 'access-control-allow-origin': 'https://ply3r.github.io/trybe-tunes/' } });
   const requestJson = await request.json();
   return requestJson.results;
 };
